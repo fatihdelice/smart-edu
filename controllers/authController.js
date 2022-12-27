1f343f7a1fbd056a0a8a76e1e5179349cf1c8cac
Fatih Delice <delicex58@gmail.com>
2022-12-28 01:57:26 +0300

    course - student relationship

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const Category = require('../models/Category');
+const Course = require('../models/Course');
 
 exports.createUser = async (req, res) => {
   try {
@@ -22,11 +23,9 @@ exports.loginUser = async (req, res) => {
     const user = await User.findOne({ email });
     if (user) {
       bcrypt.compare(password, user.password, (err, same) => {
-        if (same) {
-          //User Session
-          req.session.userID = user._id;
-          res.status(200).redirect('/users/dashboard');
-        }
+        //User Session
+        req.session.userID = user._id;
+        res.status(200).redirect('/users/dashboard');
       });
     }
   } catch (error) {
@@ -44,11 +43,15 @@ exports.logoutUser = (req, res) => {
 };
 
 exports.getDashboardPage = async (req, res) => {
-  const user = await User.findOne({ _id: req.session.userID });
+  const user = await User.findOne({ _id: req.session.userID }).populate(
+    'courses'
+  );
   const categories = await Category.find();
+  const courses = await Course.find({ user: req.session.userID });
   res.status(200).render('dashboard', {
     page_name: 'dashboard',
     user,
     categories,
+    courses,
   });
 };
